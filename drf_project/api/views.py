51edefe82c5208c5a5baa1cfe2bb1b4d2e4cbde0
Abhishek Soundalgekar-atri <soundalg@atrihub.io>
2026-02-09 17:24:52 -0800

    Implement DRF models, serializers, ViewSets, and routers [ICTRAINING-2318]

--- drf_project/api/views.py
+++ drf_project/api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from django.contrib.auth.models import User
+from .models import Snippet
+from .serializers import SnippetSerializer, UserSerializer
 
-# Create your views here.
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows snippets to be viewed or edited.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows users to be viewed.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
